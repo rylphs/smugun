2c79aaf6a17c8df5ba0284178b6ba8d648544ccb
Raphael <rylphs@gmail.com>
2017-12-04 16:13:00 -0200

    Correcao de bugs relacionadas ao checksum e urls de nodes

---
+++ upload.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+
+<?php
+require_once "SmugmugClient.php";
+require_once "Uploader.php";
+
+
+/*$files = glob($dir+'/*.log', GLOB_BRACE);
+foreach($files as $file){
+	unlink($file);
+}
+$uploader = new Uploader($dir);
+$uploader->startProcessing();*/
+
+?>
